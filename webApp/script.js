296f31bd6002da7c26b363578acd45857cca26ac
warlen alves <warlenalves2010@hotmail.com>
2022-05-18 18:27:21 -0300

    Adote
    Adição botão modal

---
+++ webApp/script.js
@@ -0,0 +1,12 @@
+/* Modal adote*/
+
+function acao(){
+    let modal =document.querySelector('#modal-adote')
+    modal.style.display ="block"
+    console.log(modal) 
+}
+
+function fechar(){
+    let modal=document.querySelector('.modal')
+    modal.style.display ="none"
+}
