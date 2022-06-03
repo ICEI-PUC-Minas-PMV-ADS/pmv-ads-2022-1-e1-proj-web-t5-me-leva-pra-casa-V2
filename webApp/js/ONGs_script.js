f22e8a0d897f7f7ee57642fad0bede30b39aa674
JessicSSantos <jessicsilvasantos@gmail.com>
2022-06-02 23:35:31 -0300

    Atualizando-Botoes-Ongs

--- webApp/js/ONGs_script.js
+++ webApp/js/ONGs_script.js
@@ -8,29 +8,44 @@
 //             $("#exampleModal1").modal('show');
 //             console.log("USUARIO NÃ0 LOGADO");
 //         }
-//     });
-// });
+// //     });
+// // });
 
-'use strict''
+// 'use strict''
 
-const openModal = () => document.getElementById('modal')
-.classList.add('active')
+// const openModal = () => document.getElementById('modal')
+// .classList.add('active')
 
-const closeModal = () => document.getElementById('modal')
-.classList.add('active')
-
-
-
-//CRUD - create read update delete
-
-const createClient = (client) => {
-    localStorage.setItem("teste", "teste para o crud")
-}
+// const closeModal = () => document.getElementById('modal')
+// .classList.add('active')
 
 
-// eventos
-document.getElementById('exampleModal')
-.addEventListener('click', openModal)
 
-Document.getElementById('exampleModal1')
-.addEventListener('click, closeModal')
+// //CRUD - create read update delete
+
+// const createClient = (client) => {
+//     localStorage.setItem("teste", "teste para o crud")
+// }
+
+
+// // eventos
+// document.getElementById('exampleModal')
+// .addEventListener('click', openModal)
+
+// Document.getElementById('exampleModal1')
+// .addEventListener('click, closeModal')
+
+var dados = [
+
+]
+localStorage.setItem("__dados__", JSON.stringify(dados))
+renderDataInTheTable(JSON.parse(localStorage.getItem("__dados__")))
+
+function cdsanimais() {
+    if (Array.isArray(dados)) {
+
+        localStorage.setItem("__dados__", JSON.stringify(dados))
+
+        $("#tbldados tbody").html("")
+
+        dados.forEach(function (item) {
