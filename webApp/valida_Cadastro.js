c1b38609b005708f339b02770e47cb98c6ef5408
Elizangela-Oliveira <103081269+Elizangela-Oliveira@users.noreply.github.com>
2022-06-21 07:42:50 -0300

    update validações

--- webApp/valida_Cadastro.js
+++ webApp/valida_Cadastro.js
@@ -101,7 +101,7 @@ class Validator {
 
     let email = input.value;
 
-    let errorMessage = `Insira um e-mail no padrão matheus@email.com`;
+    let errorMessage = `Insira um e-mail no padrão nome@email.com`;
 
     if(!re.test(email)) {
       this.printMessage(input, errorMessage);
